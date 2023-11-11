f3d76bf8f6fa45f57d281cfdaffd47675524f400
Voronin Egor <evoroni2004@gmail.com>
2023-11-11 22:38:15 +0300

    Добавил обработку клиента
    Добавил сохранение сокета пользователя
    Добавил функцию обработки сообщений для клиента, которую запустил в потоке.

--- MessengerServer/MessengerServer/main.cpp
+++ MessengerServer/MessengerServer/main.cpp
@@ -8,10 +8,26 @@ SOCKET* Connections;
 SOCKET Listen;
 
 int ClientCount = 0;
+
+void SendMessageToClient(int ID) {
+	char* buffer = new char[1024];
+	for (;; Sleep(75)) {
+		memset(buffer, 0, sizeof(buffer));
+		if (recv(Connections[ID], buffer, 1024, NULL)) {
+			std::cout << buffer << std::endl;
+			for (int i = 0; i <= ClientCount; i++) {
+				send(Connections[i], buffer, strlen(buffer), NULL); //отправка сообщени€ о подключении всем пользовател€м
+			}
+		}
+	}
+	delete buffer;
+}
+
 int main() {
 	setlocale(LC_ALL, "rus");
 	WSAData data;
 	WORD version = MAKEWORD(2, 2);
+	//инициализаци€ сокетов
 	int res = WSAStartup(version, &data);
 	if (res != 0) {
 		return 0;
@@ -23,7 +39,7 @@ int main() {
 	Connections = (SOCKET*)calloc(64, sizeof(SOCKET));
 
 	ZeroMemory(&hints, sizeof(hints));
-
+	//настраиваем сокет
 	hints.ai_family = AF_INET;
 	hints.ai_flags = AI_PASSIVE;
 	hints.ai_socktype = SOCK_STREAM;
@@ -31,18 +47,17 @@ int main() {
 
 	getaddrinfo(nullptr, "7770", &hints, &result);
 
-	// след. 2 строчки - объ€вление сервера в локальной сети	
+	// инициализаци€ сокета, объ€вление сервера в локальной сети	
 	Listen = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
 	bind(Listen, result->ai_addr, result->ai_addrlen);
-	
-	
 	listen(Listen, SOMAXCONN);
+	
 	freeaddrinfo(result);
 
 	std::cout << "Start server.." << std::endl;
 	char m_connect[] = "Connect.."; // то что видит клиент при подключении
 	
-	//ќжидание подключени€ от клиента
+	//ќбработка клиентов
 	for (;; Sleep(75)) {
 		
 		if (Connect = accept(Listen, nullptr, nullptr)) {
@@ -50,10 +65,12 @@ int main() {
 			Connections[ClientCount] = Connect;
 			send(Connections[ClientCount], m_connect, strlen(m_connect),NULL);
 			ClientCount++;
-		}
 
+			CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)SendMessageToClient,(LPVOID)(ClientCount - 1), NULL, NULL);
+								
+		}
 
 	}
 	
-
+	return 1;
 }
