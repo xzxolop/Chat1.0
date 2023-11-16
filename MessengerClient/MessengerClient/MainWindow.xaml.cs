8d2fc72b0adb80802dd4390d3dce2c5bf7be65d6
Voronin Egor <evoroni2004@gmail.com>
2023-11-16 18:21:19 +0300

    Добавил функционал определения ip адреса
    Добавил метод CreateClientInfo - заглушка, в будущем клиент сможет вводить ip адрес через UI
    Добавил метод ParseIpFromFile который устанавливает для клиента его ip считывая с файла

--- MessengerClient/MessengerClient/MainWindow.xaml.cs
+++ MessengerClient/MessengerClient/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +23,69 @@ namespace MessengerClient
     /// </summary>
     public partial class MainWindow : Window
     {
+        static private Socket Client;
+        private IPAddress ip = null;
+        private int port = 0;
+        bool IsConnected = false;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            CreateClientInfo("146.0.1.0:55555");
+            ParseIpFromFile();
         }
+        public void ParseIpFromFile()
+        {
+            try
+            {
+                var sr = new StreamReader(@"Client_info/data_info.txt");
+                string buffer = sr.ReadToEnd();
+                sr.Close();
+                string[] connect_info = buffer.Split(':');
+                ip = IPAddress.Parse(connect_info[0]);
+                port = int.Parse(connect_info[1]);
+                Info.Text = "IP: " + connect_info[0] + '\n' + "Port: " + connect_info[1];
+            }
+            catch
+            {
+                Info.Text = "Info: Не удалось подключиться";
+            }
+        }
+
+        public void CreateClientInfo(string IPport)
+        {
+            try
+            {
+                DirectoryInfo data = new DirectoryInfo("Client_info");
+                data.Create();
+                using (var sw = new StreamWriter(@"Client_info/data_info.txt"))
+                {
+                    sw.WriteLine(IPport);
+                }
+            }
+            catch {
+                Info.Text = "Не удалось записать в файл IPport";
+            }
+        }
+
+        public void ConnectUser()
+        {
+            if(!IsConnected)
+            {
+                ConnectButton.Content = "Отключиться";
+            }
+        }
+
+        public void DisconnectUser()
+        {
+            if(IsConnected)
+            {
+                ConnectButton.Content = "Подключиться";
+            }
+        }
+
+
+
     }
 }
