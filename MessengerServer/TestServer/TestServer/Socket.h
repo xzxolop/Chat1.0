6f32ff339066910237a6887185b925ad18263cc5
Voronin Egor <evoroni2004@gmail.com>
2024-01-17 18:13:01 +0300

    Объединил функции Init и Bind в одну

--- MessengerServer/TestServer/TestServer/Socket.h
+++ MessengerServer/TestServer/TestServer/Socket.h
@@ -10,10 +10,8 @@ class ISocketServer {//Просто функционал
 	
 public:
 	virtual void InitSocketInterfaces(int minVersion, int MaxVersion) = 0;
-	
-	virtual void InitSocket() = 0;
 
-	virtual void BindSocket(const char ipv4[], int port) = 0;
+	virtual void CreateSocket(const char ipv4[], int port) = 0;
 
 	virtual void Listen(int maxClients) = 0;
 
@@ -37,17 +35,9 @@ public:
 		}
 	}
 
-	void InitSocket() override {
-		Socket = socket(AF_INET, SOCK_STREAM, 0);
-		if (Socket == INVALID_SOCKET) {
-			std::cout << "Error init socket: " << WSAGetLastError() << std::endl;
-			closesocket(Socket);
-			WSACleanup(); // Деинициализация использования библиотеки сетевых служб (минус счётчик ссылок).
-			return;
-		}
-	}
+	void CreateSocket(const char ipv4[], int port) override {
+		InitSocket();
 
-	void BindSocket(const char ipv4[], int port) override {
 		sockaddr_in servInfo;
 		servInfo.sin_family = AF_INET;
 		servInfo.sin_port = htons(port); // htons переупаковывает unsigned short в байты понятные протоколу TCP/IP
@@ -84,6 +74,17 @@ public:
 		}
 		else {
 			std::cout << "Listening..." << std::endl;
+		}
+	}
+
+protected:
+	void InitSocket() {
+		Socket = socket(AF_INET, SOCK_STREAM, 0);
+		if (Socket == INVALID_SOCKET) {
+			std::cout << "Error init socket: " << WSAGetLastError() << std::endl;
+			closesocket(Socket);
+			WSACleanup(); // Деинициализация использования библиотеки сетевых служб (минус счётчик ссылок).
+			return;
 		}
 	}
 };
